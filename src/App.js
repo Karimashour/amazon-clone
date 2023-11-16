2b8cf5a41ea4ad7320397afddf549d43b078902b
Karimashour <77418051+Karimashour@users.noreply.github.com>
2023-11-16 16:33:22 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { React, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Checkout from "./components/Checkout";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Orders from "./components/Orders";
+import Payment from "./components/Payment";
+import { useAuth } from "./context/GlobalState";
+import { auth } from "./firebase";
+
+const App = () => {
+  const { dispatch } = useAuth();
+  const stripePromise = loadStripe(
+    "pk_test_51OCN3gBTTXVNhmezGzBYF4U9eph26iroWxlEbRO8gNPN3F9btVUoH6ZRFYGJQk97IvPNYqyHdMil2gmE0Cov3fpA004CGhU5c3"
+  );
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+  return (
+    <div className="app">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home/>
+            </>
+          }
+        />
+        <Route
+          path="/checkout"
+          element={
+            <>
+              <Header />
+              <Checkout/>
+            </>
+          }
+        />
+        <Route
+          path="/payment"
+          element={
+            <>
+              <Header />
+              <Elements stripe={stripePromise}>
+                <Payment/>
+              </Elements>
+             
+            </>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <>
+              <Header />
+              <Orders/>
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
